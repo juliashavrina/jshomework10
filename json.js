a01fc8877b9c4a7f09bcf8e6e449ec0cbb5b0272
«juliashavrina» <tifanys@yandex.ru>
2022-12-21 21:09:02 +0300

    js

---
+++ json.js
@@ -0,0 +1,43 @@
+const dataJson = `[{
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Men",
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    },
+    {
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Women",
+        "gender": "Men",
+        "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    },
+    {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Women",
+        "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    },
+    {
+        "name": "Beth Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Women",
+        "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+    },
+    {
+        "name": "Jerry Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Men",
+        "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+    },
+    {
+        "name": "Abadango Cluster Princess",
+        "status": "Alive",
+        "species": "Alien",
+        "gender": "Women",
+        "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg"
+    }
+]`
